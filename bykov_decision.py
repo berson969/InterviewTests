6bacbf96c6e0aae94ecf5f8744d43377d6cfc16f
Evgeny Berson <berson969@gmail.com>
2022-08-23 19:41:30 +0200

    with Bykov decision

--- bykov_decision.py
+++ bykov_decision.py
@@ -1,38 +1,39 @@
 class Person:
-    def __init__(self, name: str):
+    def __init__(self, name):
         self.name = name
 
     def __str__(self):
-        return f'{self.name}'
+        return self.name
 
     def push_name(self, person):
         person.name = self.name
 
-    def pull_name(self, person):
-        self.name = person.name
+    def attack(self, person):
+        name = str(person)
+        for _ in str(name):
+            if _ in self.name.lower() + self.name.upper():
+                name = name.replace(_, '')
+        person.name = name
 
     def show_name(self):
         print(f"Hello, I am {self.name}")
 
-    def attack(self, person):
-        person.name = _executive(person.name, self.name, '')
-
 
 class Destroyer:
-    def __init__(self, chars: str, replace_chars='#'):
-        self.chars = chars.lower()
+    def __init__(self, chars, replace_chars='#'):
+        self.chars = chars
         self.replace_chars = replace_chars
 
     def __call__(self, person):
-        self.name = person.name
-        person.name = _executive(person.name, self.chars, self.replace_chars)
-        lost_char = "', '".join(sorted(set(self.name.lower()) - set(person.name.lower())))
-        print(f"Destroyed: '{lost_char}' from {person.name}'s name.")
+        name = str(person)
+        repl_list = []
+        for _ in str(name):
+            if _ in self.chars.lower() + self.chars.upper():
+                name = name.replace(_, self.replace_chars)
+                repl_list.append(f"'{_.lower()}'")
+        print(f"Destroyed: {', '.join(set(repl_list))} from {name}'s name.")
+        person.name = name
         return person
-
-
-def _executive(name: str, chars: str, replace_chars: str):
-    return "".join(char if char.lower() not in chars else replace_chars for char in name)
 
 
 if __name__ == '__main__':
